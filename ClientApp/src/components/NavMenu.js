73a738ff45365af0d3371eb8678370b3941a862c
NIALL-2020-PC\niall <niall.garratt@gmail.com>
2023-03-22 18:45:42 +0000

    cleanup nav menu

--- ClientApp/src/components/NavMenu.js
+++ ClientApp/src/components/NavMenu.js
@@ -4,22 +4,6 @@ import { Link } from 'react-router-dom';
 /* import './NavMenu.css'; */
 
 export class NavMenu extends Component {
-  static displayName = NavMenu.name;
-
-  constructor(props) {
-    super(props);
-
-    this.toggleNavbar = this.toggleNavbar.bind(this);
-    this.state = {
-      collapsed: true
-    };
-  }
-
-  toggleNavbar() {
-    this.setState({
-      collapsed: !this.state.collapsed
-    });
-  }
 
   render() {
     return (
@@ -27,8 +11,7 @@ export class NavMenu extends Component {
         <Navbar className="navbar" container light>
           <NavbarBrand tag={Link} to="/" className='navbarbrand'>Niall's Recipes</NavbarBrand>
           <div className='navbarlinks'>
-            <NavLink tag={Link} className="navlink" to="/">Home</NavLink>
-            <NavLink tag={Link} className="navlink" to="/counter">Counter</NavLink>
+            <NavLink tag={Link} className="navlink" to="/about">About</NavLink>
             <NavLink tag={Link} className="navlink" to="/fetch-data">Fetch data</NavLink>
             <NavLink tag={Link} className="navlink" to="/rank-items">Rank items</NavLink>
             <NavLink tag={Link} className="navlink" to="/fetch-recipes">Recipies</NavLink>
