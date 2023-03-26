9661f9c685a6af4d074d0dad2e446b0e58a78b8a
NIALL-2020-PC\niall <niall.garratt@gmail.com>
2023-03-26 23:30:47 +0100

    started design for the recipe page

--- ClientApp/src/components/Recipe.js
+++ ClientApp/src/components/Recipe.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import {Link, useParams} from 'react-router-dom';
+import MovieImageArr from './MovieImages';
 
 const Recipe = () => {
     const {id} = useParams();
@@ -26,8 +27,34 @@ const Recipe = () => {
             {recipe != null ? 
             <div>            
                 <h2>{recipe.title}</h2>
-                <div>calories: {recipe.calories}</div>
-                <div>time: {recipe.time}</div>
+
+                <img src={MovieImageArr.find(o => o.id === recipe.imageId)?.image} alt={recipe.title} className='recipe-img'/>
+                <div className='recipe-info-boxes'>
+                    <div className='recipe-preview-price-wrapper'>
+                        <div className='recipe-preview-price'>
+                            {recipe.calories}Kcal
+                        </div>
+                    </div>
+
+                    <div className='recipe-preview-time-wrapper'>
+                        <div className='recipe-preview-time'>
+                            {recipe.time} mins
+                        </div>
+                    </div>
+                </div>
+                {/* <p>{recipe.description}</p> */}
+                <h2 style={{borderTop: "3px solid #7768fc94"}}>Ingredients</h2>
+{/*                 <ul>
+                    {recipe.ingredients.map((ingredient, index) => (
+                    <li key={index}>{ingredient}</li>
+                    ))}
+                </ul>
+                <h2>Instructions</h2>
+                <ol>
+                    {recipe.instructions.map((instruction, index) => (
+                    <li key={index}>{instruction}</li>
+                    ))}
+                </ol> */}
             </div>
 
 
