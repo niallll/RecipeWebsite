58533f965343cd927af55f43a8b97aab233d6898
NIALL-2020-PC\niall <niall.garratt@gmail.com>
2023-04-30 19:22:35 +0100

    displaying instructions work

--- ClientApp/src/components/Recipe.js
+++ ClientApp/src/components/Recipe.js
@@ -1,16 +1,16 @@
 import React, { useState, useEffect } from 'react';
-import {Link, useParams} from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import MovieImageArr from './MovieImages';
 
 const Recipe = () => {
-    const {id} = useParams();
+    const { id } = useParams();
 
     const [recipe, setRecipe] = useState(null);
 
     const [isEditing, setIsEditing] = useState(false);
 
     function EditClick() {
-      setIsEditing(!isEditing);
+        setIsEditing(!isEditing);
     }
 
     function CancelClick() {
@@ -18,11 +18,10 @@ const Recipe = () => {
         setIsEditing(!isEditing);
         this.forceUpdate();
     }
-    
+
     useEffect(() => {
         fetch(`recipe/${id}`)
             .then((results) => {
-                alert(results.id);
                 return results.json();
             })
             .then(data => {
@@ -34,73 +33,73 @@ const Recipe = () => {
 
     return (
         <main>
-            {recipe != null ? 
-            <div>            
-                <h2>{recipe.title}</h2>
+            {recipe != null ?
+                <div>
+                    <h2>{recipe.title}</h2>
 
-                <div style={{display:"flex", marginBottom:"40px"}}>   
+                    <div style={{ display: "flex", marginBottom: "40px" }}>
 
-                    <img src={MovieImageArr.find(o => o.id === recipe.imageId)?.image} alt={recipe.title} className='recipe-img'/>
-                    <div className='recipe-description-area'>
-                        <div>{recipe.description}</div>
+                        <img src={MovieImageArr.find(o => o.id === recipe.imageId)?.image} alt={recipe.title} className='recipe-img' />
+                        <div className='recipe-description-area'>
+                            <div>{recipe.description}</div>
 
-                        <div className='recipe-info-boxes'>
-                            <div className='recipe-preview-price-wrapper'>
-                                <div className='recipe-preview-price'>
-                                    {recipe.calories} Kcal
+                            <div className='recipe-info-boxes'>
+                                <div className='recipe-preview-price-wrapper'>
+                                    <div className='recipe-preview-price'>
+                                        {recipe.calories} Kcal
+                                    </div>
+                                </div>
+
+                                <div className='recipe-preview-time-wrapper'>
+                                    <div className='recipe-preview-time'>
+                                        {recipe.time} mins
+                                    </div>
                                 </div>
                             </div>
 
-                            <div className='recipe-preview-time-wrapper'>
-                                <div className='recipe-preview-time'>
-                                    {recipe.time} mins
-                                </div>
-                            </div>
                         </div>
-                        
+
+
                     </div>
 
+                    <div className='recipe-ingredient-instructions'>
+                        <div>
+                            <h2>Ingredients</h2>
+                            <ul className='my-list'>
+                                {recipe.ingredients != null &&
+                                    recipe.ingredients.map((ingredient, index) => (
+                                        <li key={index}>{ingredient}</li>
+                                    ))}
+                            </ul>
+                        </div>
 
-                </div>
-
-                <div className='recipe-ingredient-instructions'>
-                    <div>
-                        <h2>Ingredients</h2>
-                        <ul className='my-list'>
-                            {recipe.ingredients != null && 
-                            recipe.ingredients.map((ingredient, index) => (
-                            <li key={index}>{ingredient}</li>
-                            ))}
-                        </ul>
+                        <div className='list-instructions-wrapper'>
+                            <h2>Instructions</h2>
+                            <ol className='list-instructions'>
+                                {recipe.instructions && recipe.instructions.sort((a, b) => a.stepNumber - b.stepNumber).map((instruction, index) => (
+                                    <li key={index}>{instruction.instructionText}</li>
+                                ))}
+                            </ol>
+                        </div>
                     </div>
 
-                    <div className='list-instructions-wrapper'>
-                        <h2>Instructions</h2>
-                        <ol className='list-instructions'>
-                            {/*{recipe.instructions && recipe.instructions.map((instruction, index) => (*/}
-                            {/*<li key={index}>{instruction}</li>*/}
-                            {/*))}*/}
-                        </ol>
-                    </div>
-                </div>
-                
-                {/* <EditableLabel label="asas" isEditing={isEditing}></EditableLabel> */}
+                    {/* <EditableLabel label="asas" isEditing={isEditing}></EditableLabel> */}
 
-                <div className='recipe-ingredient-instructions'>
-                    <Link to={`/recipe-edit/${recipe.id}`} style={{ textDecoration: 'none' }}>
-                        <button className="edit-button">Edit</button>
-                    </Link>
+                    <div className='recipe-ingredient-instructions'>
+                        <Link to={`/recipe-edit/${recipe.id}`} style={{ textDecoration: 'none' }}>
+                            <button className="edit-button">Edit</button>
+                        </Link>
 
-{/*                     {!isEditing && <button onClick={EditClick} className="edit-button">Edit</button>}
+                        {/*                     {!isEditing && <button onClick={EditClick} className="edit-button">Edit</button>}
                     {isEditing && <button onClick={CancelClick} className="edit-button">Cancel</button>}
                     {isEditing && <button onClick={EditClick} className="edit-button">Save</button>} */}
+                    </div>
+
                 </div>
 
-            </div>
 
-
-            : <div>Loading...</div>}
+                : <div>Loading...</div>}
         </main>
-    ) 
+    )
 }
 export default Recipe;
