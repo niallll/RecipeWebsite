5d413ad7f7168751004dbd5edeceba6cb9163604
NIALL-2020-PC\niall <niall.garratt@gmail.com>
2023-06-26 22:51:33 +0100

    refactor recipe context

--- reactapp/src/components/Recipies.js
+++ reactapp/src/components/Recipies.js
@@ -1,42 +1,14 @@
 import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
-import {
-  Card,
-  CardBody,
-  CardTitle,
-  Col,
-  CardText,
-  Badge,
-  Row,
-  CardImg,
-} from "reactstrap";
 import RecipiesFilter from "./RecipiesFilter";
 import { RecipeFilterProvider } from "../contexts/RecipeFilterContext";
 import RecipeCards from "./RecipeCards";
 
 const Recipies = () => {
-  const [items, setItems] = useState(null);
-
-  useEffect(() => {
-    fetch(`api/recipe`)
-      .then((results) => {
-        console.log(results);
-        return results.json();
-      })
-      .then((data) => {
-        console.log(data);
-        setItems(data);
-      })
-      .catch((e) => {
-        console.log(e);
-      });
-  }, []);
-
   return (
     <RecipeFilterProvider>
       <h2>Recipes</h2>
       <RecipiesFilter></RecipiesFilter>
-      <RecipeCards items={items}></RecipeCards>
+      <RecipeCards></RecipeCards>
     </RecipeFilterProvider>
   );
 };
